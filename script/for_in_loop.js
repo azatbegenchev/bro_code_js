34064ac32044fec081f5c0da36ffd89c59bc3d20
Azat Begenchev <azat.begenchev@yandex.ru>
2021-11-23 16:38:12 +0500

    first

---
+++ script/for_in_loop.js
@@ -0,0 +1,28 @@
+
+// for in loop = Loops through the elements in an array
+//               or the properties of an Object
+//               less syntax to write, but less flexible
+
+let letters = ["H","E","L","P"];
+
+/*for(let i = 0; i < letters.length; i++){
+    console.log(letters[i]);
+}
+*/
+
+for (let i in letters) {
+    console.log(letters[i]);
+    
+}
+
+let Car = {
+    make: "Chevy",
+    model: "Corvette",
+    year: 2022,
+    color: "blue"
+}
+
+for (let property in Car) {
+    console.log(Car[property]);
+    
+}
