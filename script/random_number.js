46fdc9b087e02f09af0a1b175f9e843f9c059ad3
Azat Begenchev <azat.begenchev@yandex.ru>
2021-11-25 11:31:22 +0500

    random

--- script/random_number.js
+++ script/random_number.js
@@ -2,6 +2,6 @@ var randNum;
 
 // randNum = Math.random() //0 - 1
 // randNum =  Math.floor(Math.random() * 6); // 0 - 5
-randNum =  Math.floor(Math.random() * 6) + 1;
+randNum =  Math.floor(Math.random() * 6) + 1; //1 - 6
 
 console.log(randNum)
