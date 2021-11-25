4648dc731d3bf8038f29a523e106f510b3ce61d2
Azat <azat.begenchev@yandex.ru>
2021-11-25 20:50:38 +0500

    str-met

--- script/string_methods.js
+++ script/string_methods.js
@@ -1,7 +1,26 @@
 var myName = "Azat Begenchev";
+var myStreet = "2-pr. Lermontowa 13";
+var myCity = "Ashgabat Ahal 993";
+var myPhone = "99312-90-68-47"
 
 
 console.log(myName.length);
 console.log(myName.charAt(5))
 console.log(myName.indexOf("e"))
 console.log(myName.lastIndexOf("B"));
+console.log(myName.trim());
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+
+// var firstName = myName.slice(0, 4);
+var firstName = myName.slice(0, myName.indexOf(" "));
+console.log(firstName);
+
+var lastName = myName.slice(myName.lastIndexOf(" ") +1);
+console.log(lastName);
+
+var myAddress = myStreet.concat(" ", myCity);
+console.log(myAddress)
+
+myPhone = myPhone.replaceAll("-","");
+console.log(myPhone);
