1f0392f66b462ea2899c1e1827727258c3b1ce7d
Azat Begenchev <azat.begenchev@yandex.ru>
2021-11-25 11:49:59 +0500

    ra

--- script/random_number.js
+++ script/random_number.js
@@ -1,7 +1,21 @@
+// Math.random() = generates a pseudo-random between 0 and 1
+//                 Don't use this for security purposes
+
 var randNum;
 
 // randNum = Math.random() //0 - 1
 // randNum =  Math.floor(Math.random() * 6); // 0 - 5
-randNum =  Math.floor(Math.random() * 6) + 1; //1 - 6
+// randNum =  Math.floor(Math.random() * 6) + 1; //1 - 6
 
-console.log(randNum)
+function getRandomNum(min, max){
+    return Math.floor(Math.random() * (max - min + 1))
+}
+
+randNum = getRandomNum(1, 1000000);
+console.log(randNum);
+
+randNum = getRandomNum(1, 2000000);
+console.log(randNum);
+
+randNum = getRandomNum(1, 3000000);
+console.log(randNum);
