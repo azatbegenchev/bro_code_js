cad6ce2820d6e7e76487cd5914aeb7ce9e001060
Azat Begenchev <azat.begenchev@yandex.ru>
2021-11-26 21:28:22 +0500

    break

---
+++ script/break_continue.js
@@ -0,0 +1,16 @@
+
+// for (let i = 1; i <= 20; i++) {
+    
+//     if (i == 13)  {
+//         break
+// }
+//     console.log(i);
+// };
+
+for (let i = 1; i <= 20; i++) {
+    
+    if (i == 15)  {
+        continue;
+}
+    console.log(i);
+};
